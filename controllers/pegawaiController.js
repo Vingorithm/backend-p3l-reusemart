683c3edafeba73f017ce4ee176a59751a29569d7
NightOwlLab <ivantjandra18@gmail.com>
2025-06-10 05:04:23 +0700

    [FEAT] new API for pegawai

--- controllers/pegawaiController.js
+++ controllers/pegawaiController.js
@@ -297,7 +297,14 @@ exports.getAkunByPegawaiId = async (req, res) => {
 
 exports.getPegawaiByIdAkun = async (req, res) => {
   try {
-    const pegawai = await Pegawai.findOne({ where: { id_akun: req.params.id } });
+    const pegawai = await Pegawai.findOne({ 
+      where: { id_akun: req.params.id },
+      include: [
+        {
+          model: Akun
+        }
+      ]
+    });
     if (!pegawai) return res.status(404).json({ message: 'Pegawai tidak ditemukan' });
     res.status(200).json(pegawai);
   } catch (error) {
