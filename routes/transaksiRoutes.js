bad9b9d6bc0ce636af018247c424e0d8ce1b6204
Richard Angelico Pudjohartono <220711747@students.uajy.ac.id>
2025-06-11 01:51:55 +0700

    [ADD] Done Hunter Mobile

--- routes/transaksiRoutes.js
+++ routes/transaksiRoutes.js
@@ -8,5 +8,7 @@ router.get('/', transaksiController.getAllTransaksi);
 router.get('/:id', transaksiController.getTransaksiById);
 router.put('/:id', transaksiController.updateTransaksi);
 router.delete('/:id', transaksiController.deleteTransaksi);
+router.get('/hunter/:id_hunter', transaksiController.getTransaksiByHunterId);
+router.get('/hunter/:id_hunter/summary', transaksiController.getHunterKomisiSummary);
 
 module.exports = router;
